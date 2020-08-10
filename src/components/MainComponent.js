1e1d2517ed77c71a543b56e0d111345e7e355030
Nilesh Jain <njain.we@gmail.com>
2020-08-10 18:52:11 +0530

    links in breadcrum added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import { LEADERS } from '../shared/leaders';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import DishDetail from './DishdetailComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
 
@@ -35,12 +36,20 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
         return (
             <div>
                 <Header />
                 <Switch>
                   <Route path='/home' component={HomePage} />
                   <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                  <Route path='/menu/:dishId' component={DishWithId} />
                   <Route exact path='/contactus' component={Contact} />
                   <Redirect to="/home" />
                 </Switch>
